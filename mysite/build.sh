a49063e94ec58cddffd4965e7eeed18bcbea907d
Scott Carey <117527854+ScottCarey22@users.noreply.github.com>
2024-04-29 09:29:47 -0400

    Update build.sh

--- mysite/build.sh
+++ mysite/build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3 install -r requirements.txt
+pip3 install -r mysite/mysite/requirements.txt
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input
