1918c3944295058de1695ce0c30086257d04176d
Scott Carey <117527854+ScottCarey22@users.noreply.github.com>
2024-05-14 16:26:19 -0400

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,6 +118,24 @@ USE_I18N = True
 USE_TZ = True
 
 
+# logging debugs
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'ERROR',
+    }
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
